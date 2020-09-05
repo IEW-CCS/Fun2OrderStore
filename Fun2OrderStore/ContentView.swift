a039c3e01dae4e9c5478449f2902bdb50f093039
James <robohood83@gmail.comgit config --global user.name james>
2020-09-05 08:45:27 +0800

    Initial Commit

---
+++ Fun2OrderStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fun2OrderStore
+//
+//  Created by Lo Fang Chou on 2020/9/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
